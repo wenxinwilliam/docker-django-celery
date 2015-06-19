d63075bb1a7e8077605b5c66cdcf3f0fa5184151
William Wen <projects.wx@gmail.com>
2015-06-18 22:28:11 -0400

    added: aiohttp websocket multiple coroutines, next - use queue server to notify websocket response for celery tasks

--- myaiohttpapp/testserver.py
+++ myaiohttpapp/testserver.py
@@ -1,7 +1,9 @@
 import asyncio
 from aiohttp import web
 import time
+import os
 
+WS_FILE = os.path.join(os.path.dirname(__file__), 'websocket-error.html')
 
 @asyncio.coroutine
 def handle(request):
@@ -12,20 +14,38 @@ def handle(request):
 
 @asyncio.coroutine
 def wshandler(request):
-    ws = web.WebSocketResponse()
-    ws.start(request)
+    resp = web.WebSocketResponse()
+    
+    ok, protocol = resp.can_start(request)
+    if not ok:
+        with open(WS_FILE, 'rb') as fp:
+            return web.Response(body=fp.read(), content_type='text/html')
+
+    resp.start(request)
+
+    request.app['sockets'].append(resp)
 
     while True:
-        msg = yield from ws.receive()
+        msg = yield from resp.receive()
 
         if msg.tp == web.MsgType.text:
-            ws.send_str("Hello, {}".format(msg.data))
+            resp.send_str("Hello, {}".format(msg.data))
         elif msg.tp == web.MsgType.binary:
-            ws.send_bytes(msg.data)
+            resp.send_bytes(msg.data)
         elif msg.tp == web.MsgType.close:
+            request.app['sockets'].remove(resp)
             break
 
-    return ws
+    return resp
+
+
+@asyncio.coroutine
+def ws_ping(sockets, seconds):
+    while True:
+        a = yield from asyncio.sleep(seconds)
+        for socket in sockets:
+            socket.send_str('websocket server ping')
+
 
 @asyncio.coroutine
 def sleep_handler(request):
@@ -44,6 +64,7 @@ def sleep_handler(request):
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
+    app['sockets'] = []
     app.router.add_route('GET', '/echo', wshandler)
     app.router.add_route('GET', '/sleep/{seconds}', sleep_handler)
     app.router.add_route('GET', '/{name}', handle)
@@ -51,8 +72,14 @@ def init(loop):
     srv = yield from loop.create_server(app.make_handler(),
                                         '0.0.0.0', 8000)
     print("Server started at http://0.0.0.0:8000")
-    return srv
+
+    yield from ws_ping(app['sockets'], 2)
+    return srv, app
+
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+srv, app = loop.run_until_complete(init(loop))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    loop.close()
